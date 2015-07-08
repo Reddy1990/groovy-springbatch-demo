32c5bb3c00829e25a81c081899373bd354db99ae
amr <dquijote22@gmail.com>
2015-07-08 12:44:19 +0200

    build2

--- philae-data/build/resources/main/logback.groovy
+++
@@ -1,8 +0,0 @@
-appender("stdout", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) {
-	pattern = "%d [%thread] [%level] %logger - %msg%n"
-  }
-}
-logger("org.springframework.batch", INFO)
-logger("com.cucharitas.book", INFO)
-root(WARN, ["stdout"])
