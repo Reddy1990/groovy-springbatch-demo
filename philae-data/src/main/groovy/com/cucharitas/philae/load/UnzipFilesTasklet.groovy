4905b40e13b0ae00165c2e47761f9f67ba1d44a1
amr <dquijote22@gmail.com>
2015-07-08 12:43:12 +0200

    tasklet update

--- philae-data/src/main/groovy/com/cucharitas/philae/load/UnzipFilesTasklet.groovy
+++ philae-data/src/main/groovy/com/cucharitas/philae/load/UnzipFilesTasklet.groovy
@@ -11,37 +11,49 @@ import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
+
+import java.io.BufferedOutputStream
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipEntry
 
 import groovy.util.logging.Slf4j
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
 
 @Slf4j
 @Component("unzipFilesTasklet")
 public class UnzipFilesTasklet implements Tasklet{
 
-    private final static String INPUT_FILE_PATH = "inputDir/files.output.tar.gz"
+    private final static String INPUT_FILE_PATH = "inputDir/files.output.zip"
 
     private final static String OUTPUT = "tmpFiles"
-
 
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext){
         Resource inputRes = new ClassPathResource(INPUT_FILE_PATH)
         Resource outputRes = new ClassPathResource(OUTPUT)
 
-        log.info "loaded resources"
-
-        final TarArchiveInputStream tarIS = new TarArchiveInputStream(inputRes.getInputStream())
+        assert inputRes.exists()
         assert outputRes.exists()
-        FileUtils.cleanDirectory(outputRes.getFile())
 
-        tarIS.each{entry ->
-            String filename = entry.getName()
-            log.info "unzipping $filename"
-            byte[] content = new byte[(int) entry.getSize()]
-            tarIs.read(content, offset, content.length - offset)
-            File outputFile = new FileOutputStream(outputRes.createRelative(filename).getFile())
-            outputFile.write(content)
+        final ZipInputStream zis = new ZipInputStream(
+            new BufferedInputStream(inputRes.getInputStream()));
+
+        // Copies each item from zipped file to output
+        BufferedOutputStream dest = null;
+        ZipEntry ze = zis.getNextEntry();
+        while (ze != null) {
+            //final File target = outputRes.createRelative(ze.getName()).getFile()
+            final File target = new File(outputRes.getFile(), ze.getName())
+
+            log.error target.getPath();
+
+            final FileOutputStream fos = new FileOutputStream(target);
+            dest = new BufferedOutputStream(fos);
+            IOUtils.copy(zis, dest);
+            dest.flush();
+            dest.close();
+            ze = zis.getNextEntry();
         }
+        zis.close();
 
         RepeatStatus.FINISHED
     }
